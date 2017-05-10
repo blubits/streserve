c1eb63d9badde0266608307b5787ae0ecac7718b
Maded Batara III <mnbatara3@gmail.com>
2017-05-10 09:13:44 +0800

    Added chemical reserve functionality
    + Chemicals can now be successfully procured
    + SQLAlchemy to JSON now works!
    + Chemical and equipment logs now accessible through /<type>/logs
    + Log URLs can now be accessed from chemical descriptions and vv.
    + Other stuff

--- app.py
+++ app.py
@@ -7,14 +7,15 @@ API for the STReserve application.
 
 from flask import Flask
 from flask import jsonify
+from flask import request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS, cross_origin
-from datetime import datetime
+import datetime
 
 app = Flask(__name__)
 CORS(app)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
@@ -29,15 +30,9 @@ class Chemical(db.Model):
     name = db.Column(db.String)
     state = db.Column(db.Boolean)
     qty = db.Column(db.Integer)
-    logs = db.relationship("ChemicalLog", back_populates="chemical")
-
-    def __init__(self, name, state, qty):
-        self.name = name
-        self.state = state
-        self.qty = qty
 
     def __repr__(self):
-        return "<Chemical {0}, {1}{2}>".format(
+        return "<Chemical '{0}', {1}{2}>".format(
             self.name, self.qty, "g" if self.state else "mL")
 
 class Equipment(db.Model):
@@ -47,72 +42,73 @@ class Equipment(db.Model):
     name = db.Column(db.String)
     is_consumable = db.Column(db.Boolean)
     qty = db.Column(db.Integer)
-    logs = db.relationship("EquipmentLog", back_populates="equipment")
-
-    def __init__(self, name, is_consumable, qty):
-        self.name = name
-        self.is_consumable = is_consumable
-        self.qty = qty
 
     def __repr__(self):
-        return "<Equipment {0}{1}, qty {2}>".format(
+        return "<Equipment '{0}'{1}, qty {2}>".format(
             self.name, " (consumable)" if self.is_consumable else "", self.qty)
 
 class ChemicalLog(db.Model):
     __tablename__ = 'chemical_log'
 
     id = db.Column(db.Integer, primary_key=True)
-    group_code = db.Column(db.Integer)
     chemical_id = db.Column(db.Integer, db.ForeignKey("chemicals.id"))
-    chemical = db.relationship("Chemical", back_populates="logs")
+    chemical = db.relationship("Chemical", backref="logs")
+    group_code = db.Column(db.Integer)
     qty = db.Column(db.Integer)
     date_procured = db.Column(db.DateTime)
-
-    def __init__(self, group_code, chemical, qty, date_procured):
-        self.group_code = group_code
-        self.chemical = chemical
-        self.qty = qty
-        self.date_procured = date_procured
 
 class EquipmentLog(db.Model):
     __tablename__ = 'equipment_log'
 
     id = db.Column(db.Integer, primary_key=True)
-    group_code = db.Column(db.Integer)
     equipment_id = db.Column(db.Integer, db.ForeignKey("equipment.id"))
-    equipment = db.relationship("Equipment", back_populates="logs")
+    equipment = db.relationship("Equipment", backref="logs")
+    group_code = db.Column(db.Integer)  
     qty = db.Column(db.Integer)
     date_procured = db.Column(db.DateTime)
     date_return = db.Column(db.DateTime)
 
-    def __init__(self, group_code, equipment, qty, date_procured, date_return):
-        self.group_code = group_code
-        self.equipment = equipment
-        self.qty = qty
-        self.date_procured = date_procured
-        self.date_return = date_return
-
 class ChemicalSchema(ma.ModelSchema):
     class Meta:
         model = Chemical
+    logs = ma.List(ma.HyperlinkRelated('get_chemical_log_by_id'))
 
 class EquipmentSchema(ma.ModelSchema):
     class Meta:
         model = Equipment
+    logs = ma.List(ma.HyperlinkRelated('get_equipment_log_by_id'))
 
-# chemical_schema = ChemicalSchema()
-# equipment_schema = EquipmentSchema()
+class ChemicalLogSchema(ma.ModelSchema):
+    class Meta:
+        model = ChemicalLog
+    chemical = ma.HyperlinkRelated('get_chemical_by_id')
 
-# chemicals = []
-# chemicals.append(Chemical("Sodium chloride", True, 200))
-# chemicals.append(Chemical("Water", False, 100))
-# equipment_sample = Equipment("Petri dish (small)", False, 15)
+class EquipmentLogSchema(ma.ModelSchema):
+    class Meta:
+        model = EquipmentLog
+    equipment = ma.HyperlinkRelated('get_equipment_by_id')
 
-# db.create_all()
-# for chemical in chemicals:
-#     db.session.add(chemical)
-# db.session.add(equipment_sample)
-# db.session.commit()
+chemicals = []
+chemicals.append(Chemical(name="Sodium chloride", state=True, qty=200))
+chemicals.append(Chemical(name="Water", state=False, qty=100))
+
+equipment = []
+equipment.append(Equipment(name="Petri dish (small)", 
+    is_consumable=False, qty=15))
+
+db.reflect()
+db.drop_all()
+db.create_all()
+for chemical in chemicals:
+    db.session.add(chemical)
+for equip in equipment:
+    db.session.add(equip)
+db.session.commit()
+ 
+chemical_schema = ChemicalSchema()
+equipment_schema = EquipmentSchema()
+chemical_log_schema = ChemicalLogSchema()
+equipment_log_schema = EquipmentLogSchema()
 
 #########
 ## APP ##
@@ -127,70 +123,143 @@ def home():
         "version": VERSION
     })
 
-@app.route("/chemicals")
 @app.route("/chemicals/")
-def chemicals():
-    return jsonify([
-        {
-            "id": 1,
-            "name": "Sodium chloride",
-            "state": True,
-            "qty": 200
-        }
-    ])
+def get_all_chemicals():
+    result = [
+        chemical_schema.dump(chemical).data 
+        for chemical in Chemical.query.all()
+    ]
+    return jsonify(result)
 
-@app.route("/equipment")
 @app.route("/equipment/")
-def equipment():
-    return jsonify([
-        {
-            "id": 1001,
-            "name": "Petri dish",
-            "is_consumable": False,
-            "qty": 15
-        }
-    ])
+def get_all_equipment():
+    result = [
+        equipment_schema.dump(equipment).data 
+        for equipment in Equipment.query.all()
+    ]
+    return jsonify(result)
 
-@app.route("/chemicals/add")
+@app.route("/chemicals/<int:id>/")
+def get_chemical_by_id(id):
+    return jsonify(chemical_schema.dump(Chemical.query.get(id)).data)
+
+@app.route("/equipment/<int:id>/")
+def get_equipment_by_id(id):
+    return jsonify(equipment_schema.dump(Equipment.query.get(id)).data)
+
+@app.route("/chemicals/logs/")
+def get_all_chemical_logs():
+    result = [
+        chemical_log_schema.dump(chemical_log).data 
+        for chemical_log in ChemicalLog.query.all()
+    ]
+    return jsonify(result)
+
+@app.route("/equipment/logs/")
+def get_all_equipment_logs():
+    result = [
+        equipment_log_schema.dump(equipment_log).data 
+        for equipment_log in EquipmentLog.query.all()
+    ]
+    return jsonify(result)
+
+@app.route("/chemicals/logs/<int:id>")
+def get_chemical_log_by_id(id):
+    return jsonify(chemical_log_schema.dump(ChemicalLog.query.get(id)).data)
+
+@app.route("/equipment/logs/<int:id>")
+def get_equipment_log_by_id(id):
+    return jsonify(equipment_log_schema.dump(EquipmentLog.query.get(id)).data)
+
+@app.route("/chemicals/add/")
 def add_chemical():
-    pass
+    return jsonify()
 
-@app.route("/equipment/add")
+@app.route("/equipment/add/")
 def add_equipment():
     pass
 
-@app.route("/chemicals/remove")
+@app.route("/chemicals/<int:id>/remove/")
 def remove_chemical():
     pass
 
-@app.route("/equipment/remove")
+@app.route("/equipment/<int:id>/remove/")
 def remove_equipment():
     pass
 
-@app.route("/chemicals/<int:id>/update")
+@app.route("/chemicals/<int:id>/update/")
 def update_chemical(id):
     pass
 
-@app.route("/equipment/<int:id>/update")
+@app.route("/equipment/<int:id>/update/")
 def update_equipment(id):
     pass
 
-@app.route("/chemicals/<int:id>/reserve")
-def reserve_chemical(id):
-    date_reserved = request.args.get('date_reserved', None)
-    qty = request.args.get('qty', None)
-    group_code = request.args.get('group_code', None)
-    chemical = Chemical.query.filter(Chemical.id == id).first
-    log = ChemicalLog(group_code, chemical, qty, datetime(date_reserved))
+"""
+Reserve a chemical.
 
-@app.route("/equipment/<int:id>/reserve")
+URL args:
+    id (int): ID of chemical to be reserved.
+GET args:
+    groupcode (int): Group code that reserved item, in the format BBBBSSNN
+        (where B is the group's batch, S is the group's section (01 for Charm 
+        to 16 for Res2 H), and N is the group number).
+    qty (int): Amount of chemical to be reserved.
+    dateprocured (DateTime): Date that chemical was procured in ISO format
+        (YYYYMMDD).
+"""
+@app.route("/chemicals/<int:id>/reserve/")
+def reserve_chemical(id):
+    success = True
+    # Get all required stuff
+    chemical = Chemical.query.filter(Chemical.id == id).first()
+    print(chemical)
+    group_code = int(request.args.get('groupcode', None))
+    qty = int(request.args.get('qty', None))
+    date_procured = request.args.get('dateprocured', None)
+    # Convert all items to Python objects
+    try:
+        date = datetime.datetime.strptime(
+            date_procured, "%Y%m%d").date()
+        date_procured = datetime.datetime.combine(
+            date, datetime.datetime.min.time())
+    except:
+        pass
+    # Quantity check
+    if group_code is None or qty is None or date_procured is None:
+        success = False
+        error = "Too few arguments"
+    elif chemical.qty - qty < 0:
+        success = False
+        error = "Reserved too much"
+    elif qty < 0:
+        success = False
+        error = "Reserved too little"
+    elif group_code < 20180000:
+        success = False
+        error = "Unrecognized group code"
+    # Create object
+    if success:
+        log = ChemicalLog(
+            chemical=chemical, group_code=group_code, qty=qty, 
+            date_procured=date_procured)
+        chemical.qty -= qty
+        db.session.add(log)
+        db.session.commit()
+        return jsonify({
+            "status": "OK",
+            "log": chemical_log_schema.dump(log).data
+        })
+    else:
+        return jsonify({
+            "status": "Error",
+            "error": error
+        })
+    
+
+@app.route("/equipment/<int:id>/reserve/")
 def reserve_equipment(id):
-    date_reserved = request.args.get('date_reserved', None)
-    date_return = request.args.get('date_return', None)
-    qty = request.args.get('qty', None)
-    group_code = request.args.get('group_code', None)
-    chemical = Chemical.query.filter(Chemical.id == id).first
-    log = ChemicalLog(group_code, chemical, qty, datetime(date_reserved), datetime(date_return))
+    pass
 
 if __name__ == '__main__':
     app.debug = True
